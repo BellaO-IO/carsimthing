90bb2cf0e2497fcbca7e33c7d23a55ae746e6cc5
bella <theboss@ciaobella.me>
2021-06-25 09:54:29 -0700

    first commit

---
+++ ConsoleApp15/Motorcycle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp15
+{
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle()
+        {
+           
+        }
+
+        public bool HasSideCart { get; set; } = false; 
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This motorcycle is in drive");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is a {GetType().BaseType.Name} that is virtually in drive");
+        }
+
+    }
+}
+   
+
+
+
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,61 @@
+﻿using ConsoleApp15;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Vehicles
+            // Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
+            /*
+             * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
+             * - new it up as one of each derived class
+             * Set the properties with object initializer syntax
+             */
+            var focus = new Car() { HasTrunk = true, Make = "ford", Model = "focus", Year = 2013 };
+
+            var motorcycle = new Motorcycle() { HasSideCart = true, Make = "HD", Model = "Chopper", Year = 1978 };
+
+            Vehicle vehicle1 = new Car() { HasTrunk = true, Make = "dodge", Model = "sedan", Year = 1988 };
+
+            Vehicle vehicle2 = new Car() { HasTrunk = false, Make = "Panamera", Model = "porche", Year = 2080 };
+            /*
+             * Using a foreach loop iterate over each of the properties
+             */
+            vehicles.Add(vehicle1);
+            vehicles.Add(vehicle2);
+            vehicles.Add(focus);
+            vehicles.Add(motorcycle);
+           
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine(".......");
+                Console.WriteLine(vehicle.Make);
+                Console.WriteLine(vehicle.Model);
+                Console.WriteLine(vehicle.Year);
+
+            }
+
+            focus.DriveAbstract();
+            focus.DriveVirtual();
+            motorcycle.DriveVirtual();
+            motorcycle.DriveAbstract();
+            vehicle1.DriveAbstract();
+            vehicle1.DriveVirtual();
+
+
+            // Call each of the drive methods for one car and one motorcycle
+
+            #endregion            
+            Console.ReadLine();
+        }
+    }
+}
